09ab92c7d1a8b84b1916e7cce9a57788aeecb530
Jeffrey Ackerman <ackerman.jeffrey@gmail.com>
2023-03-15 00:33:47 -0600

    setting up server js with class repo as a guide

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const connectDB = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // TODO: Add MySQL password here
+    password: 'password',
+    database: 'employee_info_db'
+  },
+  console.log(`Connected to the employee database.`)
+);
