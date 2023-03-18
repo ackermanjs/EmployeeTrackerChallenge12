ed84f89dafb1e8c667ee0e4b13a9da1c3cffffd5
Jeffrey Ackerman <ackerman.jeffrey@gmail.com>
2023-03-17 19:41:40 -0600

    i was doing my add and commit from the db file so i wasn't actually getting the server js file, so heres that

--- server.js
+++ server.js
@@ -1,23 +1,103 @@
-const express = require('express');
-const mysql = require('mysql2');
-const inquirer = require('inquirer');
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
 
 const PORT = process.env.PORT || 3001;
-const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 // Connect to database
 const connectDB = mysql.createConnection(
   {
-    host: 'localhost',
+    host: "localhost",
     // MySQL username,
-    user: 'root',
+    user: "root",
     // TODO: Add MySQL password here
-    password: 'password',
-    database: 'employee_info_db'
-  },
-  console.log(`Connected to the employee database.`)
+    password: "password",
+    database: "employee_info_db",
+  }
+  //console.log(`Connected to the employee database.`)
 );
+
+connectDB.connect((err) => {
+  if (err) {
+    console.log(err);
+    res.status(500);
+    return res.send("Unable to connect to the DB.");
+  }
+  console.log("Connected to the Employee Database!");
+
+  startPrompt();
+});
+
+startPrompt = () => {
+  inquirer.prompt({
+      name: "mainMenu",
+      type: "list",
+      message: "How would you like to proceed?",
+      choices: [
+        "View Employees",
+        "View Departments",
+        "View Roles",
+        "Exit the Application",
+      ],
+    })
+    .then(function (result) {
+      switch (result.option) {
+        case "View Employees":
+          accessEmployees();
+          break;
+        case "View Departments":
+          accessDepartments();
+          break;
+        case "View Roles":
+          accessRoles();
+          break;
+        default:
+          exitPrompt();
+      }
+    });
+};
+
+const accessDepartments = () => {
+  connectDB.query(`SELECT * FROM departments;`, (err, data) =>{
+    if (err) throw err;
+    console.log(data);
+    console.table(data);
+    startPrompt();
+  });
+};
+
+const accessRoles = () => {
+  connectDB.query(`SELECT roles.role_id, roles.title, departments.name AS department, roles.salary 
+  FROM roles 
+  INNER JOIN departments 
+  ON roles.dept_id = departments.dept_id;`, 
+  (err, data) => {
+    if (err) throw err;
+    console.table(data);
+    startPrompt();
+  });
+};
+
+const accessEmployees = () => {
+  connectDB.query( `SELECT employees.employee_id, employees.first_name, employees.last_name, roles.title, departments.name AS department, roles.salary, CONCAT(managers.first_name, " ", managers.last_name) AS manager
+  FROM roles
+  INNER JOIN employees
+  ON roles.role_id = employees.role_id
+  INNER JOIN departments
+  ON roles.dept_id = departments.dept_id
+  LEFT JOIN employees managers
+  ON managers.employee_id = employees.manager_id
+  ORDER BY employee_id;`,
+  (err, data) => {
+    if (err) throw err;
+    console.table(data);
+    startPrompt();
+  }
+  );
+}
+
+
+const exitPrompt = () => {
+  connectDB.end;
+  process.exit;
+};
